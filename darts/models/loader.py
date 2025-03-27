223aea9bd320a0e6e3b928606e7b9efe142472b0
Sam Lyddon <slyddon@alvarezandmarsal.com>
2025-03-27 15:46:51 +0000

    Refine lazy loader

--- darts/models/loader.py
+++ darts/models/loader.py
@@ -1,38 +1,61 @@
+import functools
 import importlib
+import operator
 import os
 from types import ModuleType
-from typing import Any, Dict, Optional, Set
+from typing import Any, Optional
 
 from darts.models.utils import NotImportedModule
 
-IMPORT_STRUCTURE_T = Dict[str, Dict[str, Set[str]]]
+IMPORT_STRUCTURE_T = dict[str, dict[str, set[str]]]
 
 
 class LazyLoader(ModuleType):
-    """Lazy module loader."""
+    """Module class that surfaces all objects but only performs associated
+    imports when the objects are requested.
 
+    """
+
+    # Very heavily inspired by optuna.integration._IntegrationModule
+    # https://github.com/optuna/optuna/blob/master/optuna/integration/__init__.py
+    # transformers.file_utils._LazyModule
+    # https://github.com/huggingface/transformers/blob/master/src/transformers/file_utils.py
     def __init__(
         self,
         name: str,
         module_file: str,
         import_structure: IMPORT_STRUCTURE_T,
         module_spec: Optional[importlib.machinery.ModuleSpec] = None,
+        extra_objects: Any = None,
     ) -> None:
         super().__init__(name)
-
-        self.__file__ = module_file
-        self.__path__ = [os.path.dirname(module_file)]
-        self.__spec__ = module_spec
-
         self._modules = set(import_structure.keys())
-
-        _class_to_module = {}
+        self._class_to_module = {}
         for key, values in import_structure.items():
             for value in values:
-                _class_to_module[value] = key
-        self._class_to_module = _class_to_module
+                self._class_to_module[value] = key
+
+        self.__file__ = module_file
+        self.__spec__ = module_spec
+        self.__path__ = [os.path.dirname(module_file)]
+        self._objects = {} if extra_objects is None else extra_objects
+        self._name = name
+        self._import_structure = import_structure
+
+        # needed for autocompletion in an IDE
+        self.__all__ = (
+            list(import_structure.keys())
+            + functools.reduce(operator.iadd, import_structure.values(), [])
+            + list(self._objects.keys())
+        )
+
+    # needed for autocompletion in an IDE
+    def __dir__(self):
+        return super().__dir__() + self.__all__
 
     def __getattr__(self, name: str) -> Any:
+        if name in self._objects:
+            return self._objects[name]
         if name in self._modules:
             value = self._get_module(name)
         elif name in self._class_to_module:
@@ -49,3 +72,6 @@ class LazyLoader(ModuleType):
             return importlib.import_module(module_name, self.__name__)
         except ModuleNotFoundError:
             return NotImportedModule(module_name=self.__name__, warn=False)
+
+    def __reduce__(self):
+        return (self.__class__, (self._name, self.__file__, self._import_structure))
